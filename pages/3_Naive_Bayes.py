d0f608cf5cdc9fea95236736a899253340b97f68
AinaRezal <ainarezal.27@gmail.com>
2024-03-25 20:33:03 +0800

    Update 3_Naive_Bayes.py

--- pages/3_Naive_Bayes.py
+++ pages/3_Naive_Bayes.py
@@ -1,49 +1,49 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 import pickle
 from sklearn.naive_bayes import GaussianNB
 
 st.set_page_config(layout="wide",
                    initial_sidebar_state="collapsed",
-                   page_title='Neural Network'
+                   page_title='Naive Bayes'
                    )
 
-st.markdown("<h1 style='text-align: center; color: black;'>Neural Network</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Naive Bayes</h1>", unsafe_allow_html=True)
 
 st.write('Write. This will be where the description of what will be predicted.')
 st.text('Text. This will be where the description of what will be predicted.')
 st.markdown("""---""")
 
+# Pickle for prediction and mapping values
+nb_pickle = open('nbm.pickle', 'rb')
+map_pickle = open('outputnb.pickle', 'rb')
 
-df_file = st.file_uploader('Upload Earthquake dataset')
+nbm = pickle.load(nb_pickle)
+mapping = pickle.load(map_pickle)
 
-if df_file is None: 
-    # Pickle for prediction and mapping values
-    nb_pickle = open('nbm.pickle', 'rb')
-    map_pickle = open('outputnb.pickle', 'rb')
+nb_pickle.close()
+map_pickle.close()
 
-    nbm = pickle.load(nb_pickle)
-    mapping = pickle.load(map_pickle)
+df = pd.read_csv('cleaned_earthquake.csv')
+features = df[['magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'nst', 'latitude', 'longitude']]
+output = df['alert']
 
-    nb_pickle.close()
-    map_pickle.close()
-    
-else:
-    df = pd.read_csv('cleaned_earthquake.csv')
-    
-    features = df[['magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'nst', 'latitude', 'longitude']]
-    output = df['alert']
-    output, mapping = pd.factorize(output)
+x_train, x_test, y_train, y_test = train_test_split(features,
+                                                    output,
+                                                    test_size= 0.2,
+                                                    random_state=42)
 
-    x_train, x_test, y_train, y_test = train_test_split(features,
-                                                        output,
-                                                        test_size= 0.2,
-                                                        random_state=42)
+nbmodel = GaussianNB()
+nbmodel.fit(x_train, y_train)
+y_pred = nbmodel.predict(x_test)
+accuracy = accuracy_score(y_pred, y_test)
+f1score = f1_score(y_test, y_pred, average='weighted')
+st.write('Accuracy:', accuracy)
+st.write('f1 score:', f1score)
 
-    nbm = GaussianNB()
-    nbm.fit(x_train, y_train)
-    
 
 with st.form('inputs'):
     # 'magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'latitude', 'longitude'
