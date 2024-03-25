7f5adbd8d8ff8325d09ef71fd0033efd746356f7
AinaRezal <ainarezal.27@gmail.com>
2024-03-26 05:03:04 +0800

    update

--- nb.py
+++ nb.py
@@ -1,22 +1,23 @@
+# Importing necessary libraries
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import confusion_matrix
-import pickle
-import os
 import matplotlib.pyplot as plt
 import seaborn as sns 
+import pickle
+import os
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
 from sklearn.inspection import permutation_importance
 
 # Directory
 curDir = os.getcwd()
 
-# Reading csv file
+# Reading earthquake file
 df = pd.read_csv('cleaned_earthquake.csv')
 
-# Choosing columns
+# Choosing columns for training and prediction
 features = df[['magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'nst', 'latitude', 'longitude']]
 output = df['alert']
 output, mapping = pd.factorize(output)
@@ -27,35 +28,37 @@ x_train, x_test, y_train, y_test = train_test_split(features,
                                                     test_size=0.2,
                                                     random_state=41)
 
-# # Model
+# Modelling
 nbmodel = GaussianNB()
 nbmodel.fit(x_train, y_train)
 y_pred = nbmodel.predict(x_test)
+
+# To see how prediction can be reliable
 accuracy = accuracy_score(y_pred, y_test)
-f1score = f1_score(y_test, y_pred, average='weighted')
-cfmatrix = confusion_matrix(y_test, y_pred)
+f1score = round(f1_score(y_test, y_pred, average='weighted'), 2)
+precision = round(precision_score(y_test, y_pred, average='weighted'), 2)
 print('Accuracy:', accuracy)
 print('f1 score:', f1score)
+print('Precision:', precision)
 
+# Creating file to store trained model
 model_path = os.path.join(curDir, 'nbm.pickle')
 with open(model_path, 'wb') as nb_pickle:
     pickle.dump(nbmodel, nb_pickle)
     nb_pickle.close()
-    
+
+# Creating file to store output    
 model_path2 = os.path.join(curDir, 'outputnb.pickle')
 with open(model_path2, 'wb') as outputnb_pickle:
     pickle.dump(mapping, outputnb_pickle)
     outputnb_pickle.close()
 
+# Finding out the each column's importance for prediction
 imp = permutation_importance(nbmodel, x_test, y_test)
 fig, ax = plt.subplots() 
 ax = sns.barplot(x=imp.importances_mean, y=features.columns) 
 plt.title('Which features are the most important for species prediction?') 
-plt.xlabel('Importance') 
-plt.ylabel('Feature') 
+plt.xlabel('Importance of Features') 
+plt.ylabel('Features of Earthquake') 
 plt.tight_layout() 
-fig.savefig('feature_importance.png')
-
-fig2, ax2 = plt.subplots()
-ax2 = sns.heatmap(cfmatrix, annot=True, fmt='d', cmap='Blues', cbar=False)
-fig2.savefig('confusion_matrix.png')
+fig.savefig('features_importance.png')
--- pages/3_Naive_Bayes.py
+++ pages/3_Naive_Bayes.py
@@ -1,20 +1,43 @@
+# Importing necessary libraries
 import streamlit as st
 import pandas as pd
+import pickle
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-import pickle
-from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import precision_score
+from nb import accuracy, precision, f1score
 
+# Setting page configuration
 st.set_page_config(layout="wide",
                    initial_sidebar_state="collapsed",
                    page_title='Naive Bayes'
                    )
-
 st.markdown("<h1 style='text-align: center; color: black;'>Naive Bayes</h1>", unsafe_allow_html=True)
 
-st.write('Write. This will be where the description of what will be predicted.')
-st.text('Text. This will be where the description of what will be predicted.')
+# Description of Model
+con1 = st.container(border=True)
+col = con1.columns(2)
+
+with col[0]:
+    table_info = pd.DataFrame(
+        {"Column's Name" : ['magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'nst', 'latitude', 'longitude'],
+        'Description' : ['Size of earthquake.',
+                        'Estimated instrument intensity during the event of earthquake.',
+                        'Oceanic event.',
+                        'Horizontal distance from the surface of the epicentre to the nearest station.',
+                        'Largest azimuthally gap between two azimuthally adjacent stations.',
+                        'The location of the ruptures (epicentre).',
+                        'Number of seismic stations.',
+                        'Coordinates',
+                        'Coordinates.']}
+    )
+    st.table(table_info)
+
+with col[1]:
+    st.image('features_importance.png', caption='Importance of Each Feature For Prediction') # How to centralize
+
 st.markdown("""---""")
 
 # Pickle for prediction and mapping values
@@ -27,42 +50,48 @@ mapping = pickle.load(map_pickle)
 nb_pickle.close()
 map_pickle.close()
 
+
+# Pre-model and modelling process
 df = pd.read_csv('cleaned_earthquake.csv')
 features = df[['magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'nst', 'latitude', 'longitude']]
 output = df['alert']
-
 x_train, x_test, y_train, y_test = train_test_split(features,
                                                     output,
                                                     test_size= 0.2,
                                                     random_state=42)
-
 nbmodel = GaussianNB()
 nbmodel.fit(x_train, y_train)
 y_pred = nbmodel.predict(x_test)
-accuracy = accuracy_score(y_pred, y_test)
-f1score = f1_score(y_test, y_pred, average='weighted')
-st.write('Accuracy:', accuracy)
-st.write('f1 score:', f1score)
 
+# To show reliability for users
+con2 = st.container(border=True)
+# accuracy = accuracy_score(y_pred, y_test)
+# f1score = round(f1_score(y_test, y_pred, average='weighted'), 2)
+# precision = round(precision_score(y_test, y_pred, average='weighted'), 2)
+reliabilty_info = pd.DataFrame(
+    {'Evaluation Metrics' : ['Accuracy', 'Precision', 'F1score'],
+     'Description' : ['The percentage that correctly predicts an output out of all predictions.',
+                     'Quality of correct predictions.',
+                     'The percentage that measures accuracy by the total times it predicts correctly throughout the whole dataset.'],
+     'Percentage' : [accuracy, precision, f1score]}
+)
+st.table(reliabilty_info)
 
+# User inputs
 with st.form('inputs'):
     # 'magnitude', 'mmi', 'tsunami', 'dmin', 'gap', 'depth', 'latitude', 'longitude'
-    magnitude = st.slider('Magnitude', 0.0, 10.0, 5.0, step=0.1)
-    mmi = st.number_input('Estimated Instrument Intensity', min_value=0)    
+    magnitude = st.slider('Magnitude of Earthquake', 0.0, 10.0, 5.0, step=0.1)
+    mmi = st.number_input('Estimated Instrument Intensity during Earthquake', min_value=0)    
     tsunami = st.number_input('Occurence of Tsunami', min_value=0, max_value=1)
-    distance = st.number_input('Horizontal Distance (km)', min_value=0)    
-    gap = st.number_input('Gap of stations (degrees)', min_value=0, max_value=360)
-    depth = st.number_input('Depth in kilometers', min_value=0)
+    distance = st.number_input('Horizontal Distance (km) From Surface of Epicentre To Nearest Station', min_value=0)    
+    gap = st.number_input('Largest Gap Between Two Azimuthally Stations (degrees)', min_value=0, max_value=360)
+    depth = st.number_input('Depth of Epicentre (km)', min_value=0)
     stations = st.number_input('Number of Seismic Stations', min_value=0)
-    lang = st.number_input('Latitude', min_value=-90.00, max_value=90.00)
+    lat = st.number_input('Latitude', min_value=-90.00, max_value=90.00)
     long = st.number_input('Longitude', min_value=-180.00, max_value=180.00)
     submit = st.form_submit_button()
 
 if submit :
-    prediction = nbm.predict([[magnitude, mmi, tsunami, distance, gap, depth, stations, lang, long]])
-    prediction_alert = mapping[prediction] # Takes the highest possibility of the prediction
-    print(prediction)
-
+    prediction = nbm.predict([[magnitude, mmi, tsunami, distance, gap, depth, stations, lat, long]])
+    prediction_alert = mapping[prediction][0] # Takes the highest possibility of the prediction
     st.write(prediction_alert)
-    # something is wrong with mapping
-    # st.write(prediction_alert)
